1fbbeb8b40776be1e0141ff62898df7d312e8fd7
MattijnStolk <mattijn001@gmail.com>
2023-05-10 23:18:46 +0200

    started on css

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log("hello")
+function init() {
+    let contactButton = document.querySelector('#contact-us')
+    contactButton.addEventListener("click", contactHandler)
+}
+
+function contactHandler() {
+    console.log("quack")
+}
+
+window.addEventListener("load", init)
