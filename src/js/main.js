52490984678a6ada0741db7e6fa21c1fa17710a1
AmyRom <1015403@hr.nl>
2023-05-22 21:50:54 +0200

    Style changes

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ function contactHandler() {
     console.log("quack")
     const email = [
         '0986087@hr.nl',
-        '1015402@hr.nl',
+        '1015403@hr.nl',
         '1010682@hr.nl',
         '1015903@hr.nl'
     ]
